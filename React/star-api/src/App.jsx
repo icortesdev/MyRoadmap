b0fb215a979bb38a0c2aa5c621dc5718be185f2d
Irian <icortesdev@proton.me>
2024-09-09 19:40:47 +0200

    Proyecto Star Wars Characters.

---
+++ React/star-api/src/App.jsx
@@ -0,0 +1,93 @@
+import { useState, useEffect, useRef } from "react";
+import "./App.css";
+import { getPeople, getCharacter, searchCharacter } from "./api/people";
+
+function App() {
+  const inputSearch = useRef(null);
+  const [textSearch, setTextSearch] = useState("");
+
+  const [people, setPeople] = useState([]);
+  const [currentCharacter, setCurrentCharacter] = useState(1);
+  const [details, setDetails] = useState({});
+
+  const [page, setPage] = useState(1);
+
+  const [errorState, setErrorState] = useState({ hasError: false });
+
+  useEffect(() => {
+    getPeople(page).then(setPeople).catch(handleError);
+  }, [page]);
+
+  useEffect(() => {
+    getCharacter(currentCharacter).then(setDetails).catch(handleError);
+  }, [currentCharacter]);
+
+  const handleError = (err) => {
+    setErrorState({ hasError: true, message: err.message });
+  };
+
+  const showDetails = (character) => {
+    const id = Number(character.url.split("/").slice(-2)[0]);
+    setCurrentCharacter(id);
+  };
+
+  const onChangeTextSearch = (event) => {
+    event.preventDefault();
+    const text = inputSearch.current.value;
+    setTextSearch(text);
+  };
+
+  const onSearchSubmit = (event) => {
+    if (event.key !== "Enter") return;
+
+    inputSearch.current.value = "";
+    setDetails({});
+    searchCharacter(textSearch).then(setPeople).catch(handleError);
+  };
+
+  const onChangePage = (next) => {
+    if (!people.previous && page + next <= 0) return;
+    if (!people.next && page + next >= 9) return;
+
+    setPage(page + next);
+  };
+
+  return (
+    <>
+<h1>Star Wars Characters</h1>
+      <input
+        ref={inputSearch}
+        onChange={onChangeTextSearch}
+        onKeyDown={onSearchSubmit}
+        type="text"
+        placeholder="search your favorite"
+      />
+      <ul>
+        {errorState.hasError && <div>{errorState.message}</div>}
+        {people?.results?.map((character) => (
+          <li key={character.name} onClick={() => showDetails(character)}>
+            {character.name}
+          </li>
+        ))}
+      </ul>
+
+      <section>
+        <button onClick={() => onChangePage(-1)}>Prev</button>| {page} |
+        <button onClick={() => onChangePage(1)}>Next</button>
+      </section>
+
+      {details && (
+        <aside>
+          <h1>{details.name}</h1>
+          <ul>
+            <li>height: {details.height}</li>
+            <li>mass: {details.mass}</li>
+            <li>Year of Birth: {details.birth_year}</li>
+          </ul>
+        </aside>
+      )}
+    </>
+  );
+}
+
+export default App;
